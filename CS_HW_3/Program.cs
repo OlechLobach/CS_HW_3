ad6c5cc8aa2e6336e6000609232e6da5d39e6580
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-15 02:29:59 +0200

    initiall commit

--- CS_HW_3/Program.cs
+++ CS_HW_3/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter two numbers:");
+        if (int.TryParse(Console.ReadLine(), out int number1) && int.TryParse(Console.ReadLine(), out int number2))
+        {
+            
+            int start = Math.Min(number1, number2);
+            int end = Math.Max(number1, number2);
+
+            Console.WriteLine($"Even numbers between {start} and {end}:");
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter two integers.");
+        }
+    }
+}
