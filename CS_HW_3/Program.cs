562e4b78e66d25fd60852595ea0431cfe12e06a5
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-15 02:42:54 +0200

    initiall commit

--- CS_HW_3/Program.cs
+++ CS_HW_3/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter two positive integers A and B (A < B):");
+        if (int.TryParse(Console.ReadLine(), out int A) && int.TryParse(Console.ReadLine(), out int B) && A < B)
+        {
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter two positive integers where A < B.");
+        }
+    }
+}
