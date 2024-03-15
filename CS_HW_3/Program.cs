ac17b1217f548dc72f369a7251fcbe9ad49255e9
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-15 02:25:23 +0200

    initiall commit

--- CS_HW_3/Program.cs
+++ CS_HW_3/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a date (format: dd.mm.yyyy):");
+        string input = Console.ReadLine();
+
+        if (DateTime.TryParseExact(input, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+        {
+            string season = GetSeason(date.Month);
+            string dayOfWeek = date.DayOfWeek.ToString();
+
+            Console.WriteLine($"{season} {dayOfWeek}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid date format. Please enter the date in the format dd.mm.yyyy.");
+        }
+    }
+
+    static string GetSeason(int month)
+    {
+        switch (month)
+        {
+            case 12:
+            case 1:
+            case 2:
+                return "Winter";
+            case 3:
+            case 4:
+            case 5:
+                return "Spring";
+            case 6:
+            case 7:
+            case 8:
+                return "Summer";
+            case 9:
+            case 10:
+            case 11:
+                return "Autumn";
+            default:
+                return "Unknown";
+        }
+    }
+}
