0c46593d31540c0f31fde13e5f33b3e3672d66cd
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-15 02:38:04 +0200

    initiall commit

--- CS_HW_3/Program.cs
+++ CS_HW_3/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a six-digit number:");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            int reversedNumber = ReverseNumber(number);
+            Console.WriteLine($"Reversed number: {reversedNumber}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a six-digit number.");
+        }
+    }
+
+    static int ReverseNumber(int num)
+    {
+        int reversed = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reversed = (reversed * 10) + digit;
+            num /= 10;
+        }
+        return reversed;
+    }
+}
