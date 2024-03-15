aa2007b53042421ef4d858aaafce86e00eaa95a3
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-15 02:14:22 +0200

    initiall commit

--- CS_HW_3/Program.cs
+++ CS_HW_3/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter two numbers separated by space:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        if (numbers.Length == 2 && double.TryParse(numbers[0], out double value) && double.TryParse(numbers[1], out double percentage))
+        {
+            double result = (percentage / 100) * value;
+            Console.WriteLine($"Result: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter two numbers separated by space.");
+        }
+    }
+}
