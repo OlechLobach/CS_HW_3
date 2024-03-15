16af9e4927a3e453d4f2ee332ad745b2f2c94da7
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-15 02:11:28 +0200

    initiall commit

--- CS_HW_3/Program.cs
+++ CS_HW_3/Program.cs
@@ -1,1 +1,40 @@
 ﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number from 1 to 100:");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number >= 1 && number <= 100)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: The entered number is not within the range from 1 to 100.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: The entered value is not an integer.");
+        }
+    }
+}
